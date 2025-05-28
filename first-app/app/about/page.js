c90e3c03c043f348714b2d07860afef3d82c7bf2
alibahrami633 <ali.bahrami633@yahoo.com>
2025-05-28 16:12:36 +1000

    more styling

--- first-app/app/about/page.js
+++ first-app/app/about/page.js
@@ -3,7 +3,7 @@ import styles from "../page.module.css";
 export default function FirstRoute() {
   return (
     <main className={styles.page}>
-      <h1>About page</h1>
+      <h2>About page</h2>
       <p>This is the about page of our Next.js application.</p>
     </main>
   );
